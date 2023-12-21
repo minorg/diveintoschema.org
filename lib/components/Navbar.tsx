ea8edf287ee702e1149a23d615e00078c57f6f06
Minor Gordon <minorg@server.fake>
2023-12-21 18:37:14 -0500

    working on navbar

--- lib/components/Navbar.tsx
+++ lib/components/Navbar.tsx
@@ -1,26 +1,18 @@
+import {PuzzlePieceIcon} from "@heroicons/react/24/solid";
+
 export default function Navbar() {
   return (
-    <nav className="mb-6 flex flex-wrap items-center justify-between text-green-800 ">
-      <div className="mr-6 mt-4 flex flex-shrink-0 items-center">
-        <img
-          alt="Brunswick UNITED logo"
-          className="mr-2 w-64 fill-current"
-          src="/img/color-logo.webp"
-        />
-      </div>
-      <div className="block flex-grow text-2xl">
-        <a
-          href="/"
-          className="mr-8 block  hover:text-green-900  lg:inline-block"
-        >
-          Home
+    <nav className="mb-6 flex items-center mt-4">
+      <div className="flex items-center">
+        <PuzzlePieceIcon className="mr-2 w-6" />
+        <a href="/" className="mr-8 block lg:inline-block text-xl">
+          Dive Into Schema.org
         </a>
-        {/* <a
-          href="/candidates"
-          className="block hover:text-green-900 lg:inline-block"
-        >
-          Our Candidates
-        </a> */}
+      </div>
+      <div className="flex-grow text-xl">
+        <a href="/candidates" className="blocklg:inline-block">
+          Other Page
+        </a>
       </div>
     </nav>
   );
