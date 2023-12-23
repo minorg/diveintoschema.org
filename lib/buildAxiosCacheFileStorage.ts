964a742de813e35cfd657039cedc4409f7c0d605
Minor Gordon <minorg@server.fake>
2023-12-23 09:04:31 -0500

    deterministic stringify

--- lib/buildAxiosCacheFileStorage.ts
+++ lib/buildAxiosCacheFileStorage.ts
@@ -2,18 +2,8 @@ import {buildStorage} from "axios-cache-interceptor";
 import fs from "node:fs/promises";
 import fsSync from "node:fs";
 import path from "node:path";
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const basex = require("base-x");
-const base58 = basex(
-  "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
-);
-
-export const decodeFileName = (value: string): string =>
-  base58.decode(value).toString("utf-8");
-
-export const encodeFileName = (value: string): string =>
-  base58.encode(Buffer.from(value, "utf-8"));
+import encodeFileName from "./encodeFileName";
+import stringify from "json-stringify-deterministic";
 
 export default function buildAxiosCacheFileStorage(directoryPath: string) {
   fsSync.mkdirSync(directoryPath, {recursive: true});
@@ -39,7 +29,7 @@ export default function buildAxiosCacheFileStorage(directoryPath: string) {
 
     async set(key, value) {
       const filePath = path.resolve(directoryPath, encodeFileName(key));
-      await fs.writeFile(filePath, JSON.stringify(value));
+      await fs.writeFile(filePath, stringify(value, {space: "  "}));
     },
   });
 }
