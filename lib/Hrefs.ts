f9b5bf06bb863b4af83b19568b9c3eaa2c755a2f
Minor Gordon <minorg@server.fake>
2023-12-28 16:05:26 -0500

    fix Hrefs

--- lib/Hrefs.ts
+++ lib/Hrefs.ts
@@ -4,10 +4,10 @@ export default class Hrefs {
   }
 
   static type({name}: {name: string}) {
-    return Hrefs.types + name;
+    return Hrefs.types + "/" + name;
   }
 
   static get types() {
-    return "/types/";
+    return "/types";
   }
 }
